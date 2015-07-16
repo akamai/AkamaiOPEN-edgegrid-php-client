3bff78548d3873abf38b61b374e5ec6e677a05a4
Davey Shafik <me@daveyshafik.com>
2015-07-15 22:40:24 -0400

    Reorder methods a bit

--- src/Client.php
+++ src/Client.php
@@ -135,18 +135,6 @@ class Client implements \GuzzleHttp\ClientInterface
     }
 
     /**
-     * Set the HTTP request timeout
-     *
-     * @param $timeout_in_seconds
-     */
-    public function setTimeout($timeout_in_seconds)
-    {
-        $this->optionsHandler->setTimeout($timeout_in_seconds);
-        
-        return $this;
-    }
-
-    /**
      * Proxy calls smartly to the \GuzzleHttp\Client
      *
      * @param string $method
@@ -259,7 +247,6 @@ class Client implements \GuzzleHttp\ClientInterface
         return $this;
     }
 
-
     /**
      * Specify the headers to include when signing the request
      *
@@ -294,6 +281,17 @@ class Client implements \GuzzleHttp\ClientInterface
         return $this;
     }
 
+    /**
+     * Set the HTTP request timeout
+     *
+     * @param $timeout_in_seconds
+     */
+    public function setTimeout($timeout_in_seconds)
+    {
+        $this->optionsHandler->setTimeout($timeout_in_seconds);
+
+        return $this;
+    }
     /**
      * Print formatted JSON responses to STDOUT
      *
@@ -424,89 +422,6 @@ class Client implements \GuzzleHttp\ClientInterface
         }
         
         return true;
-    }
-
-    /**
-     * Send an HTTP request.
-     *
-     * @param RequestInterface $request Request to send
-     * @param array $options Request options to apply to the given
-     *                                  request and to the transfer.
-     *
-     * @return ResponseInterface
-     * @throws GuzzleException
-     */
-    public function send(RequestInterface $request, array $options = [])
-    {
-        return $this->__call(__FUNCTION__, [$request, $options]);
-    }
-
-    /**
-     * Asynchronously send an HTTP request.
-     *
-     * @param RequestInterface $request Request to send
-     * @param array $options Request options to apply to the given
-     *                                  request and to the transfer.
-     *
-     * @return PromiseInterface
-     */
-    public function sendAsync(RequestInterface $request, array $options = [])
-    {
-        return $this->__call(__FUNCTION__, [$request, $options]);
-    }
-
-    /**
-     * Create and send an HTTP request.
-     *
-     * Use an absolute path to override the base path of the client, or a
-     * relative path to append to the base path of the client. The URL can
-     * contain the query string as well.
-     *
-     * @param string $method HTTP method
-     * @param string|UriInterface $uri URI object or string.
-     * @param array $options Request options to apply.
-     *
-     * @return ResponseInterface
-     * @throws GuzzleException
-     */
-    public function request($method, $uri, array $options = [])
-    {
-        return $this->__call(__FUNCTION__, [$method, $uri, $options]);
-    }
-
-    /**
-     * Create and send an asynchronous HTTP request.
-     *
-     * Use an absolute path to override the base path of the client, or a
-     * relative path to append to the base path of the client. The URL can
-     * contain the query string as well. Use an array to provide a URL
-     * template and additional variables to use in the URL template expansion.
-     *
-     * @param string $method HTTP method
-     * @param string|UriInterface $uri URI object or string.
-     * @param array $options Request options to apply.
-     *
-     * @return PromiseInterface
-     */
-    public function requestAsync($method, $uri, array $options = [])
-    {
-        return $this->__call(__FUNCTION__, [$method, $uri, $options]);
-    }
-
-    /**
-     * Get a client configuration option.
-     *
-     * These options include default request options of the client, a "handler"
-     * (if utilized by the concrete client), and a "base_uri" if utilized by
-     * the concrete client.
-     *
-     * @param string|null $option The config option to retrieve.
-     *
-     * @return mixed
-     */
-    public function getConfig($option = null)
-    {
-        return $this->__call(__FUNCTION__, [$option]);
     }
 
     /**
@@ -678,5 +593,88 @@ class Client implements \GuzzleHttp\ClientInterface
         }
         
         return false;
+    }
+
+    /**
+     * Send an HTTP request.
+     *
+     * @param RequestInterface $request Request to send
+     * @param array $options Request options to apply to the given
+     *                                  request and to the transfer.
+     *
+     * @return ResponseInterface
+     * @throws GuzzleException
+     */
+    public function send(RequestInterface $request, array $options = [])
+    {
+        return $this->__call(__FUNCTION__, [$request, $options]);
+    }
+
+    /**
+     * Asynchronously send an HTTP request.
+     *
+     * @param RequestInterface $request Request to send
+     * @param array $options Request options to apply to the given
+     *                                  request and to the transfer.
+     *
+     * @return PromiseInterface
+     */
+    public function sendAsync(RequestInterface $request, array $options = [])
+    {
+        return $this->__call(__FUNCTION__, [$request, $options]);
+    }
+
+    /**
+     * Create and send an HTTP request.
+     *
+     * Use an absolute path to override the base path of the client, or a
+     * relative path to append to the base path of the client. The URL can
+     * contain the query string as well.
+     *
+     * @param string $method HTTP method
+     * @param string|UriInterface $uri URI object or string.
+     * @param array $options Request options to apply.
+     *
+     * @return ResponseInterface
+     * @throws GuzzleException
+     */
+    public function request($method, $uri, array $options = [])
+    {
+        return $this->__call(__FUNCTION__, [$method, $uri, $options]);
+    }
+
+    /**
+     * Create and send an asynchronous HTTP request.
+     *
+     * Use an absolute path to override the base path of the client, or a
+     * relative path to append to the base path of the client. The URL can
+     * contain the query string as well. Use an array to provide a URL
+     * template and additional variables to use in the URL template expansion.
+     *
+     * @param string $method HTTP method
+     * @param string|UriInterface $uri URI object or string.
+     * @param array $options Request options to apply.
+     *
+     * @return PromiseInterface
+     */
+    public function requestAsync($method, $uri, array $options = [])
+    {
+        return $this->__call(__FUNCTION__, [$method, $uri, $options]);
+    }
+
+    /**
+     * Get a client configuration option.
+     *
+     * These options include default request options of the client, a "handler"
+     * (if utilized by the concrete client), and a "base_uri" if utilized by
+     * the concrete client.
+     *
+     * @param string|null $option The config option to retrieve.
+     *
+     * @return mixed
+     */
+    public function getConfig($option = null)
+    {
+        return $this->__call(__FUNCTION__, [$option]);
     }
 }
