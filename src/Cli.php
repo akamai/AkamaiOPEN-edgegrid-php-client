1003b973726f09cd4182808e67fbb35bf29a1eb3
Davey Shafik <dshafik@akamai.com>
2016-10-07 23:58:59 -0700

    Update copyright years

--- src/Cli.php
+++ src/Cli.php
@@ -12,7 +12,7 @@
  * first call {@see Akamai\Open\EdgeGrid\Client->setAuth()}.
  *
  * @author Davey Shafik <dshafik@akamai.com>
- * @copyright Copyright 2015 Akamai Technologies, Inc. All rights reserved.
+ * @copyright Copyright 2016 Akamai Technologies, Inc. All rights reserved.
  * @license Apache 2.0
  * @link https://github.com/akamai-open/edgegrid-auth-php
  * @link https://developer.akamai.com
--- src/Exception/HandlerException/IOException.php
+++ src/Exception/HandlerException/IOException.php
@@ -7,7 +7,7 @@
  * as GuzzleHttp a Middleware Handlers
  *
  * @author Davey Shafik <dshafik@akamai.com>
- * @copyright Copyright 2015 Akamai Technologies, Inc. All rights reserved.
+ * @copyright Copyright 2016 Akamai Technologies, Inc. All rights reserved.
  * @license Apache 2.0
  * @link https://github.com/akamai-open/edgegrid-auth-php
  * @link https://developer.akamai.com
