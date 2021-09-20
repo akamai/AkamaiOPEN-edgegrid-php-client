cafd255965ec6a5eb927c8dbd4a3d9144d9bfc52
David Bonting <david.bonting@hotmail.com>
2021-09-20 15:00:17 +0200

    Update sendRequest to match Guzzle

--- src/Client.php
+++ src/Client.php
@@ -123,7 +123,7 @@ class Client extends \GuzzleHttp\Client implements \Psr\Log\LoggerAwareInterface
      * @return \GuzzleHttp\Promise\PromiseInterface
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
-    public function requestAsync($method, $uri = null, array $options = [])
+    public function requestAsync($method, $uri = null, array $options = []): \GuzzleHttp\Promise\PromiseInterface
     {
         $options = $this->setRequestOptions($options);
 
@@ -144,7 +144,7 @@ class Client extends \GuzzleHttp\Client implements \Psr\Log\LoggerAwareInterface
      *
      * @return \GuzzleHttp\Promise\PromiseInterface
      */
-    public function sendAsync(\Psr\Http\Message\RequestInterface $request, array $options = []): GuzzleHttp\Promise\PromiseInterface
+    public function sendAsync(\Psr\Http\Message\RequestInterface $request, array $options = []): \GuzzleHttp\Promise\PromiseInterface
     {
         $options = $this->setRequestOptions($options);
 
