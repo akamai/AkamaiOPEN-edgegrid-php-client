f6c448ccc77659a557eca617fff76f50625b357d
Stefan Heimann <s.heimann@sportradar.com>
2023-05-30 13:54:31 +0200

    bump psr/log to ^3.0 & monolog/monolog to ^3.3

--- tests/Handler/AuthenticationTest.php
+++ tests/Handler/AuthenticationTest.php
@@ -17,6 +17,8 @@ use GuzzleHttp\Psr7\Response;
 
 class AuthenticationTest extends \Akamai\Open\EdgeGrid\Tests\ClientTest
 {
+    private \Prophecy\Prophet $prophet;
+
     protected function setUp(): void
     {
         parent::setUp();
