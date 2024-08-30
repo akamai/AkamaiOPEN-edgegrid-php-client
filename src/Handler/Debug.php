9efc1ee7f5d0376bdc7fc9faf53757dcc2787643
mniemczy <mniemczy@akamai.com>
2024-08-30 10:49:55 +0200

    DXISSUE-54 Remove the copyright comment from the code files

--- src/Handler/Debug.php
+++ src/Handler/Debug.php
@@ -1,15 +1,4 @@
 <?php
-
-/**
- * Akamai {OPEN} EdgeGrid Auth Client
- *
- * @copyright Copyright 2024 Akamai Technologies, Inc. All rights reserved.
- * @license Apache 2.0
- * @link https://github.com/akamai-open/AkamaiOPEN-edgegrid-php-client
- * @link https://developer.akamai.com
- * @link https://developer.akamai.com/introduction/Client_Auth.html
- */
-
 namespace Akamai\Open\EdgeGrid\Handler;
 
 use Akamai\Open\EdgeGrid\Exception\HandlerException\IOException;
